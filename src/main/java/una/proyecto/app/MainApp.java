1976e553d4940e04164497102aa1cf0aa08b4731
Eliseo Zuñiga <davidzunigaobando@gmail.com>
2026-08-19 01:25:29 -0600

    Interfaz mejorada y css mejorado

--- src/main/java/una/proyecto/app/MainApp.java
+++ src/main/java/una/proyecto/app/MainApp.java
@@ -1,10 +1,9 @@
 package una.proyecto.app;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import una.proyecto.utils.Navigation;
 
 public class MainApp extends Application {
     public static void main(String[] args) {
@@ -13,17 +12,9 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(
-                getClass().getResource("/una/proyecto/ui/login-view.fxml")
-        );
-        Parent root = loader.load();
-
-        Scene scene = new Scene(root);
-        scene.getStylesheets().add(getClass().getResource("/una/proyecto/css/style.css").toExternalForm());
-
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Sistema de Reserva");
         primaryStage.setResizable(false);
-        primaryStage.show();
+        Navigation.navigateTo(primaryStage,
+                "/una/proyecto/ui/login-view.fxml",
+                "Sistema de Reserva");
     }
 }
