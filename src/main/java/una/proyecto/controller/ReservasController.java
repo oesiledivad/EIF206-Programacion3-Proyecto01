76d0d1240ebca9abb1fbc9a8aeb1a384b773652f
kyarita124 <kiaraagonzalez3@gmail.com>
2026-08-18 20:09:13 -0600

    visual filtros, funciones vacias handle

--- src/main/java/una/proyecto/controller/ReservasController.java
+++ src/main/java/una/proyecto/controller/ReservasController.java
@@ -1,4 +1,32 @@
 package una.proyecto.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 public class ReservasController {
+    @FXML private Button btnestadisticas;
+    @FXML private Button btncalendarizacion;
+    @FXML private Button btnpanellateral;
+    @FXML private Button btnactividades;
+    @FXML private Button btnreserva;
+    @FXML private Button btncancelarreserva;
+    @FXML private Button btnlimpiar;
+    @FXML private TextField txtfieldfrase;
+    @FXML private TextField txtfieldactividad;
+    @FXML private DatePicker datapickerfecha;
+    @FXML private ChoiceBox choiceboxhorainicio;
+    @FXML private ChoiceBox choiceboxhorafin;
+    @FXML private ListView listviewcategorias;
+    @FXML private TableView tableviewmisreservas;
+    @FXML public void initialize(){}
+    private void handleEstadisticasButton(){}
+    private void handleCalendarizacionButton(){}
+    private void handlePanelLateralButton(){}
+    private void handleActividadesButton(){}
+    private void handleReservaButton(){}
+    private void handleCancelarReservaButton(){}
+    private void handleLimpiarButton(){}
+
+
+
 }
